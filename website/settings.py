3246fd452f9b1ff69263b85a3a52a78ac7110c56
smanjil <shresthamanjil21@gmail.com>
2017-12-05 11:31:03 +0545

    revert back setting

--- website/settings.py
+++ website/settings.py
@@ -90,10 +90,6 @@ DATABASES = {
                 }
 }
 
-import dj_database_url
-
-DATABASES[‘default’] = dj_database_url.config()
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
