3056168d53bfd33042f1678bc5b74f1046e446d5
smanjil <shresthamanjil21@gmail.com>
2017-12-05 09:36:14 +0545

    database confs

--- website/settings.py
+++ website/settings.py
@@ -136,3 +136,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
