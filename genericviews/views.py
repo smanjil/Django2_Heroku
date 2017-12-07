4dc6f868595b945998b73c30c609243387270313
smanjil <shresthamanjil21@gmail.com>
2017-12-07 19:46:57 +0545

    custom 404 error page

--- genericviews/views.py
+++ genericviews/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.shortcuts import (
+    render,
+    HttpResponseRedirect, 
+    reverse
+)
 from django.views import generic
 from django.contrib import messages
 
