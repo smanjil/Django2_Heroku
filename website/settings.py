22a084e899f9e8def9d1c95e92681464405da831
smanjil <shresthamanjil21@gmail.com>
2017-12-07 19:51:11 +0545

    new settings

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8th0_@$0%=(2-b2(2b*aq2+!)8erh+fidi@3l=)a88ce$2g@dn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
